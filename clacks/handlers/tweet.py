61484edb2e0f7cfb8d56b41b76702c71087150f6
Ben Meier <benmeier42@gmail.com>
2014-07-28 20:34:41 +0200

    Added basic tweet functionality

--- clacks/handlers/tweet.py
+++ clacks/handlers/tweet.py
@@ -1,4 +1,5 @@
 import click
+from twitter import *
 
 from clacks.configuration import config
 
@@ -6,5 +7,18 @@ from clacks.configuration import config
 @click.argument('content')
 def tweet(content):
     """
+    Tweet the given content
     """
-    print 1
+    t = Twitter(auth=(
+        config['twitter.oauth_token'],
+        config['twitter.oauth_secret'],
+        config['twitter.consumer_key'],
+        config['twitter.consumer_secret'],
+    ))
+
+    content = " ".join(content)
+
+    if len(content) > 140:
+        content = content[:-3] + '...'
+
+    t.statuses.update(status=content)
