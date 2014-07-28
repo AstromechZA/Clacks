251fecd480a66458de242865ad363d80838efcff
Ben Meier <benmeier42@gmail.com>
2014-07-28 19:27:59 +0200

    Email handler done

--- clacks/handlers/email.py
+++ clacks/handlers/email.py
@@ -1,15 +1,72 @@
-import click
+import smtplib
+import socket
+from datetime import datetime
 
+import click
 from clacks.configuration import config
 
+def _validate_content(ctx, param, value):
+    if not value:
+        raise click.BadParameter("content cannot be blank")
+    return value
+
 @click.command()
-@click.argument('to')
-@click.argument('content', nargs=-1)
-def email(to, content):
+@click.argument('to_address')
+@click.option('--subject', default='Clacks Message', help="The subject line.")
+@click.argument('content', nargs=-1, callback=_validate_content)
+def email(to_address, content, subject=''):
     """
     Send plain-text content to the given TO email address. Uses the
     configured email profile.
     """
-    print to
-    print content
-    print config
+
+    print "Connecting.."
+
+    session = smtplib.SMTP(
+        config['email.smtp_server_address'],
+        int(config['email.smtp_server_port'])
+    )
+
+
+    print "Starting TLS.."
+
+    session.ehlo()
+    session.starttls()
+    session.ehlo()
+
+    print "Logging In.."
+
+    session.login(
+        config['email.username'],
+        config['email.password']
+    )
+
+    print "Building Message.."
+
+    headers = "\r\n".join([
+        "from: %s" % config['email.from_address'],
+        "subject: %s" % subject,
+        "to: %s" % to_address,
+        "mime-version: 1.0",
+        "content-type: text/html"
+    ])
+
+    body = "\r\n".join([
+        "<h2>%s</h2>" % subject,
+        "<hr>",
+        "<p>%s</p>" % " ".join(content),
+        "<hr>",
+        "<b>Hostname:</b> %s" % socket.gethostname(),
+        "<br>"
+        "<b>Time: </b> %s" % datetime.now().isoformat()
+    ])
+
+    print "Sending Message.."
+
+    session.sendmail(
+        config['email.username'],
+        to_address,
+        headers + "\r\n\r\n" + body
+    )
+
+    print "Done."
